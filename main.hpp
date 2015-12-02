668568759b8ec2812b20a77e2cba95214f8a7340
PierreHalipre <pierre.halipre@gmail.com>
2015-12-02 12:02:12 +0100

    Create main.hpp

---
+++ main.hpp
@@ -0,0 +1,35 @@
+#ifndef MAIN_HPP_INCLUDED
+#define MAIN_HPP_INCLUDED
+
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+
+using namespace std;
+using namespace sf;
+
+#include "Grille.hpp"
+#include "Chronometre.hpp"
+#include "Evenement.hpp"
+#include "Objet.hpp"
+#include "Tir.hpp"
+#include "Personnage.hpp"
+#include "Ennemi.hpp"
+#include "Decor.hpp"
+#include "Zone.hpp"
+#include "Carte.hpp"
+#include "Etat.hpp"
+#include "AStar.hpp"
+#include "Menu.hpp"
+
+#endif // MAIN_HPP_INCLUDED
